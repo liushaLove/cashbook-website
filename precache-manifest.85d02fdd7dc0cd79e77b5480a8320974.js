0914bb1519dac733e9d42a2bb60fb0b820f20fca
liushaLove <1270842602@qq.com>
2021-03-07 11:57:09 +0800

    deploy

---
+++ precache-manifest.85d02fdd7dc0cd79e77b5480a8320974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fdd5ed47a4e968fa600d67e83fb51c2",
+    "url": "/cashbook-website/cashbook.png"
+  },
+  {
+    "revision": "875ec2e219c78a4599d0",
+    "url": "/cashbook-website/css/app.1bc2e9d1.css"
+  },
+  {
+    "revision": "4da40ef05e20e6718a758b2e8f6fec21",
+    "url": "/cashbook-website/index.html"
+  },
+  {
+    "revision": "875ec2e219c78a4599d0",
+    "url": "/cashbook-website/js/app.3989fa1d.js"
+  },
+  {
+    "revision": "8454b36a4f862780071f",
+    "url": "/cashbook-website/js/chunk-vendors.d6e6c561.js"
+  },
+  {
+    "revision": "b384eafa891f01b91d307c7a728467fc",
+    "url": "/cashbook-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cashbook-website/robots.txt"
+  }
+]);
